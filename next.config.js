d216e9b9bb9d4a307b2440f20281a3dac74fd0ea
weightwisehealth <amazonmanausbrazil@gmail.com>
2026-05-19 11:23:06 -0300

    Remove next-intl plugin from config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-export default withNextIntl(nextConfig);
+export default nextConfig;
