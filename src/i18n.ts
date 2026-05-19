12168fd12fadb4bc0c054c557eb5364bdbce9187
weightwisehealth <amazonmanausbrazil@gmail.com>
2026-05-19 11:28:55 -0300

    Remove all i18n files - use simple English site

--- src/i18n.ts
+++
@@ -1,6 +0,0 @@
-import { getRequestConfig } from 'next-intl/server';
-
-export default getRequestConfig(async ({ locale }) => ({
-  locale: locale || 'en',
-  messages: (await import(`./messages/${locale || 'en'}.json`)).default,
-}));
--- src/i18n/routing.ts
+++
@@ -1,6 +0,0 @@
-import { defineRouting } from 'next-intl/routing';
-
-export const routing = defineRouting({
-  locales: ['en', 'pt', 'es', 'fr', 'de', 'it', 'ru'],
-  defaultLocale: 'en',
-});
