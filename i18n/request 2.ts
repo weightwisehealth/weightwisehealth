6fc754ce51c494e46dc893a4b97ad8d893ca8d6d
WeightWise Health <contact@weightwisehealth.com>
2026-06-08 21:03:36 -0300

    cleanup legacy files from root

--- i18n/request 2.ts
+++
@@ -1,6 +0,0 @@
-import {getRequestConfig} from 'next-intl/server';
-
-export default getRequestConfig(async ({locale}) => {
-  const messages = (await import(`../src/messages/${locale}.json`)).default;
-  return {messages};
-});
